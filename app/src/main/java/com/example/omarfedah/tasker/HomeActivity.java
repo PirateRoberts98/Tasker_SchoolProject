9e0c961f8e9e9dd0ea344156033706515e7873e0
Robert C <rconr060@uottawa.ca>
2017-11-28 19:35:14 -0500

    Moved Database Folder to main r

--- app/src/main/java/com/example/omarfedah/tasker/HomeActivity.java
+++ app/src/main/java/com/example/omarfedah/tasker/HomeActivity.java
@@ -45,7 +45,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BuildDatabase() ;
+       // BuildDatabase() ;
         createListView() ;
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
