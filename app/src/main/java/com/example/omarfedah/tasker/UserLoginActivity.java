cff3ff5967e6de00118f1447f7da94be5f856d24
Cooper Lawrence <clawr014@uottawa.ca>
2017-12-03 15:55:10 -0500

    Added authetication to user login.

--- app/src/main/java/com/example/omarfedah/tasker/UserLoginActivity.java
+++ app/src/main/java/com/example/omarfedah/tasker/UserLoginActivity.java
@@ -10,22 +10,25 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class UserLoginActivity extends AppCompatActivity {
-//test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
         Button loginBtn = findViewById(R.id.loginBtn);
+        final EditText typedPassword = (EditText) findViewById(R.id.editTaskPassword);
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                Toast.makeText(getApplicationContext(),"Sure", Toast.LENGTH_SHORT).show() ;
-
+                if (GUI.authenticateUser(GUI.activeUser, typedPassword.getText().toString()) == true) {
+                    finish();
+                    Toast.makeText(getApplicationContext(), "Logged in successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
-
 }
